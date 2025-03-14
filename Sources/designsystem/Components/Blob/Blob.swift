817ce668df673113544eac72a6531c9d54f51b9a
Emmanuel Ay <emmanuel@block21.com>
2025-03-14 10:42:04 +0100

    Tidy up

--- Sources/designsystem/Components/Blob/Blob.swift
+++ Sources/designsystem/Components/Blob/Blob.swift
@@ -2,38 +2,39 @@ import SwiftUI
 import Lottie
 
 struct Blob: View {
-    
-    @Binding var index: Int?
-    
-    let keys: [Float] = [2, 8, 15, 23, 30]
-    
+    @Binding var state: Int?
+
     var body: some View {
-        VStack {
-            LottieView(animation: .named("blob_animation", bundle: .module))
-                .playing(
-                    .fromFrame(
-                        0,
-                        toFrame: AnimationFrameTime(safeFrame()),
-                        loopMode: .playOnce))
-                .frame(width: 164, height: 164)
-        }
+        LottieView(animation: .named("blob_animation", bundle: .module))
+            .playing(
+                .fromFrame(
+                    0,
+                    toFrame: AnimationFrameTime(safeFrame()),
+                    loopMode: .playOnce
+                )
+            )
     }
-    
+
     private func safeFrame() -> Float {
-        
-        guard let index = index else { return keys[0] }
-        
-        if index >= 0, index < keys.count {
-            return keys[index]
-        } else {
-            return 0
-        }
+        let keys: [Float] = [2, 8, 15, 23, 30]
+        guard let state else { return keys[0] }
+        return (state >= 0 && state < keys.count) ? keys[state] : 0
     }
 }
 
 #Preview {
-    @Previewable @State var index: Int? = 4
-    
-    Blob(index: $index)
-    
+    @Previewable @State var index: Int? = 0
+
+    VStack(spacing: 16) {
+        Blob(state: $index)
+            .frame(width: 164, height: 164)
+
+        Picker("Animation index", selection: $index) {
+            ForEach(0..<5) {
+                Text("State \($0)").tag($0)
+            }
+        }
+        .pickerStyle(.segmented)
+    }
+    .padding(16)
 }
