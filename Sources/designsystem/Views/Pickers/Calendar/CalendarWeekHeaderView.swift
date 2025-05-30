e72b7f6cf7df01220d31c2827cb86a71985ee9ab
Emmanuel Ay <emmanuel.ay@vgregion.se>
2025-05-30 09:26:10 +0200

    Made header public

--- Sources/designsystem/Views/Pickers/Calendar/CalendarWeekHeaderView.swift
+++ Sources/designsystem/Views/Pickers/Calendar/CalendarWeekHeaderView.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
-struct CalendarWeekHeaderView: View {
+public struct CalendarWeekHeaderView: View {
     let spacing: CGFloat
 
-    init(spacing: CGFloat = 2) {
+    public init(spacing: CGFloat = 2) {
         self.spacing = spacing
     }
 
@@ -13,7 +13,7 @@ struct CalendarWeekHeaderView: View {
         Array(symbols[firstWeekdayIndex...] + symbols[..<firstWeekdayIndex])
     }
 
-    var body: some View {
+    public var body: some View {
         HStack(spacing: spacing) {
             ForEach(orderedWeekdays, id: \.self) { day in
                 Text(day.capitalized(with: .autoupdatingCurrent))
