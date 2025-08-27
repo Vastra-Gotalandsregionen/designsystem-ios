65a7a2a87de3691bcdb9f0600d2ac9c97837f212
sebwa6 <sebastian.warvsten@vgregion.se>
2025-08-27 12:00:39 +0200

    Changed to viewBuilder to skip arguments

--- Sources/designsystem/Views/TableViewRow/VGRSummeryRowView.swift
+++ Sources/designsystem/Views/TableViewRow/VGRSummeryRowView.swift
@@ -1,70 +1,70 @@
 import SwiftUI
 
-///A reusable SwiftUI row view that displays up to three customizable content views and an optional trailing icon.
-/// - Parameters:
-///   - trailingIcon: An optional trailing image
-///   - indicatorColor: Background color for the leading content
-struct VGRSummaryRowView<Content: View>: View {
+/// A reusable SwiftUI row view that displays up to five customizable content views.
+struct VGRSummaryRowView<
+    Content1: View,
+    Content2: View,
+    Content3: View,
+    Content4: View,
+    TrailingIcon: View
+>: View {
+    @ScaledMetric var contentWidth: CGFloat = 32
+    @ScaledMetric var contentHeight: CGFloat = 32
 
-    var content1: (() -> Content)? = nil
-    let content2: () -> Content
-    let content3: () -> Content
-    var trailingIcon: Image?
-    var indicatorColor: Color?
+    private let content1: () -> Content1
+    private let content2: () -> Content2
+    private let content3: () -> Content3
+    private let content4: () -> Content4
+    private let trailingIcon: () -> TrailingIcon
+
+    init(
+        @ViewBuilder content1: @escaping () -> Content1 = { EmptyView() },
+        @ViewBuilder content2: @escaping () -> Content2,
+        @ViewBuilder content3: @escaping () -> Content3,
+        @ViewBuilder content4: @escaping () -> Content4 = { EmptyView() },
+        @ViewBuilder trailingIcon: @escaping () -> TrailingIcon = { EmptyView() }
+    ) {
+        self.content1 = content1
+        self.content2 = content2
+        self.content3 = content3
+        self.content4 = content4
+        self.trailingIcon = trailingIcon
+    }
 
     var body: some View {
-        if let content1 {
+        HStack {
             content1()
-                .frame(minWidth: 32, minHeight: 32)
-                .font(.caption2)
-                .background(indicatorColor)
-        } else {
-            EmptyView()
+                .frame(minWidth: contentWidth, minHeight: contentHeight)
+            content2()
+            Spacer()
+            content3()
+            content4()
+            trailingIcon()
         }
-        content2()
-        Spacer()
-        content3()
-            .font(.title)
-            .bold()
-        if let image = trailingIcon {
-            image
-                .resizable()
-                .scaledToFit()
-                .frame(width: 16, height: 16)
-                .foregroundStyle(Color.Primary.action)
-                .padding(.trailing, 16)
-        }
-    }
-}
-
-#Preview("Everything applies") {
-    var indicationNumber: String = "7,4"
-    HStack(spacing: 4) {
-        VGRSummaryRowView(
-            content1: {
-                Text("\(indicationNumber)")
-            },
-            content2: {
-                Text("Vilken raiting får vald modell?")
-            },
-            content3: {
-                Text("3310")
-            },
-            trailingIcon: Image(systemName: "chevron.right"),
-            indicatorColor: Color.Status.successSurface
-        )
+        .padding([.leading, .trailing], 16)
     }
 }
 
 #Preview("Minimal effort") {
     HStack(spacing: 4) {
         VGRSummaryRowView(
-            content2: {
-                Text("Hur går det på en skala?")
-            },
-            content3: {
-                Text("3")
-            },
+            content2: { Text("Hur går det på en skala?") },
+            content3: { Text("3") },
+        )
+    }
+}
+
+#Preview("Everything applies") {
+    HStack(spacing: 4) {
+        VGRSummaryRowView(
+            content1: { Text("4-6")
+                    .font(.caption2)
+                    .frame(width: 32, height: 32)
+                .background(Color.Accent.limeSurface)},
+            content2: { Text("Vilken raiting får vald modell?") },
+            content3: { Text("3310") },
+            content4: { Image(systemName: "heart.fill") },
+            trailingIcon: { Image(systemName: "chevron.right") }
         )
     }
 }
