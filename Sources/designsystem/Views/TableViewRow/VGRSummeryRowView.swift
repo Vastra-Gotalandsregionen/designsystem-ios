916bc2aec3e14d601d016ac2cb9a4238686c4759
sebwa6 <sebastian.warvsten@vgregion.se>
2025-09-01 14:41:53 +0200

    Updated content names

--- Sources/designsystem/Views/TableViewRow/VGRSummeryRowView.swift
+++ Sources/designsystem/Views/TableViewRow/VGRSummeryRowView.swift
@@ -1,6 +1,5 @@
 import SwiftUI
 
-/// A reusable SwiftUI row view that displays up to five customizable content views.
 struct VGRSummaryRowView<
     Content1: View,
     Content2: View,
@@ -11,34 +10,34 @@ struct VGRSummaryRowView<
     @ScaledMetric var contentWidth: CGFloat = 32
     @ScaledMetric var contentHeight: CGFloat = 32
 
-    private let content1: () -> Content1
-    private let content2: () -> Content2
-    private let content3: () -> Content3
-    private let content4: () -> Content4
+    private let indicatorContent1: () -> Content1
+    private let textContent2: () -> Content2
+    private let valueContent3: () -> Content3
+    private let trendContent4: () -> Content4
     private let trailingIcon: () -> TrailingIcon
 
     init(
-        @ViewBuilder content1: @escaping () -> Content1 = { EmptyView() },
-        @ViewBuilder content2: @escaping () -> Content2,
-        @ViewBuilder content3: @escaping () -> Content3,
-        @ViewBuilder content4: @escaping () -> Content4 = { EmptyView() },
+        @ViewBuilder indicatorContent1: @escaping () -> Content1 = { EmptyView() },
+        @ViewBuilder textContent2: @escaping () -> Content2,
+        @ViewBuilder valueContent3: @escaping () -> Content3,
+        @ViewBuilder trendContent4: @escaping () -> Content4 = { EmptyView() },
         @ViewBuilder trailingIcon: @escaping () -> TrailingIcon = { EmptyView() }
     ) {
-        self.content1 = content1
-        self.content2 = content2
-        self.content3 = content3
-        self.content4 = content4
+        self.indicatorContent1 = indicatorContent1
+        self.textContent2 = textContent2
+        self.valueContent3 = valueContent3
+        self.trendContent4 = trendContent4
         self.trailingIcon = trailingIcon
     }
 
     var body: some View {
         HStack {
-            content1()
+            indicatorContent1()
                 .frame(minWidth: contentWidth, minHeight: contentHeight)
-            content2()
+            textContent2()
             Spacer()
-            content3()
-            content4()
+            valueContent3()
+            trendContent4()
             trailingIcon()
         }
         .padding([.leading, .trailing], 16)
@@ -48,8 +47,8 @@ struct VGRSummaryRowView<
 #Preview("Minimal effort") {
     HStack(spacing: 4) {
         VGRSummaryRowView(
-            content2: { Text("Hur går det på en skala?") },
-            content3: { Text("3") },
+            textContent2: { Text("Hur går det på en skala?") },
+            valueContent3: { Text("3") },
         )
     }
 }
@@ -57,13 +56,9 @@ struct VGRSummaryRowView<
 #Preview("Everything applies") {
     HStack(spacing: 4) {
         VGRSummaryRowView(
-            content1: { Text("4-6")
-                    .font(.caption2)
-                    .frame(width: 32, height: 32)
-                .background(Color.Accent.limeSurface)},
-            content2: { Text("Vilken raiting får vald modell?") },
-            content3: { Text("3310") },
-            content4: { Image(systemName: "heart.fill") },
+            indicatorContent1: { Image(systemName: "heart.fill") },
+            textContent2: { Text("Vilken raiting får vald modell?") },
+            valueContent3: { Text("3310") },
             trailingIcon: { Image(systemName: "chevron.right") }
         )
     }
