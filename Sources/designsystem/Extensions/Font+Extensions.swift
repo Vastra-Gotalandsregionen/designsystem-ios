aa3fb73165b6bd9842488592c14f301d3066b5b3
Emmanuel Ay <emmanuel@block21.com>
2025-09-04 14:13:27 +0200

    Font templates (extension) (#48)
    * Added most common font styles
    
    ---------
    
    Co-authored-by: Emmanuel Ay <emmanuel.ay@vgregion.se>

--- Sources/designsystem/Extensions/Font+Extensions.swift
+++ Sources/designsystem/Extensions/Font+Extensions.swift
@@ -1,8 +1,23 @@
-//
-//  File.swift
-//  designsystem
-//
-//  Created by Emmanuel Ay on 2025-09-04.
-//
+import SwiftUI
 
-import Foundation
+public extension Font {
+
+    // MARK: - Body
+    static var bodyRegular: Font { .body.weight(.regular) }
+    static var bodyMedium: Font { .body.weight(.medium) }
+    static var bodyBold: Font { .body.weight(.bold) }
+    static var bodySemibold: Font { .body.weight(.semibold) }
+
+    // MARK: - Footnote
+    static var footnoteRegular: Font { .footnote.weight(.regular) }
+    static var footnoteSemibold: Font { .footnote.weight(.semibold) }
+    static var footnoteBold: Font { .footnote.weight(.bold) }
+
+    // MARK: - Headline
+    static var headlineSemibold: Font { .headline.weight(.semibold) }
+
+    // MARK: - Title 3
+    static var title3Semibold: Font { .title3.weight(.semibold) }
+    static var title3Bold: Font { .title3.weight(.bold) }
+
+}
