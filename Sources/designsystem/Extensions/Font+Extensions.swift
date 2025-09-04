a931bd81c201fb78d30f9e7c14eaedeec4ce5167
Emmanuel Ay <emmanuel.ay@vgregion.se>
2025-09-04 14:03:03 +0200

    Made CalloutShape public

---
+++ Sources/designsystem/Extensions/Font+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  designsystem
+//
+//  Created by Emmanuel Ay on 2025-09-04.
+//
+
+import Foundation
--- Sources/designsystem/Views/Cards/Callout/VGRCalloutShape.swift
+++ Sources/designsystem/Views/Cards/Callout/VGRCalloutShape.swift
@@ -2,10 +2,11 @@ import SwiftUI
 
 /// En container-vy som applicerar stil och bakgrundsfärg baserat på `CalloutShapeVariant`.
 /// Används som det yttre skalet i en `VGRCallout` för att ge visuell kontext (t.ex. varning eller information).
-struct VGRCalloutShape<Content: View>: View {
-    
+public struct VGRCalloutShape<Content: View>: View {
+
     /// Bakgrundsfärgen för komponenten
     let backgroundColor: Color
+
     /// Innehållet som visas inom den formade bakgrunden.
     let content: Content
     
@@ -13,15 +14,15 @@ struct VGRCalloutShape<Content: View>: View {
     /// - Parameters:
     ///   - variant: Formvarianten (information eller varning).
     ///   - content: En vybyggare som tillhandahåller innehållet.
-    init(backgroundColor: Color,
-         @ViewBuilder _ content: () -> Content) {
+    public init(backgroundColor: Color = Color.Status.informationSurface,
+                @ViewBuilder _ content: () -> Content) {
         self.backgroundColor = backgroundColor
         self.content = content()
     }
     
     /// Komponerar vy-layouten med padding, bakgrund och formad stil.
-    var body: some View {
-        VStack (spacing: 16) {
+    public var body: some View {
+        VStack(spacing: 16) {
             content
         }
         .padding(16)
@@ -32,12 +33,45 @@ struct VGRCalloutShape<Content: View>: View {
 }
 
 #Preview {
-    VGRShape {
-        VGRCalloutShape(backgroundColor: Color.Primary.blueSurfaceMinimal) {
-            VGRCalloutText(header: "Hello", description: "World")
+    NavigationStack {
+        ScrollView {
+            VStack(spacing: 16) {
+                VGRCalloutShape {
+                    Text("Hello, `plain` **VGRCalloutShape**, world!")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.footnote).fontWeight(.regular)
+
+                    VGRButton(label: "Action", variant: .secondary) {
+                        print("Action pressed")
+                    }
+                }
+                .padding(.horizontal, 16)
+
+                VGRCalloutShape(backgroundColor: Color.Status.warningSurface) {
+                    Text("Hello, `plain` **VGRCalloutShape**, world!")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                    VGRButton(
+                        label: "Ändra inställningar",
+                        icon: Image(systemName: "rectangle.portrait.and.arrow.right"),
+                        variant: .primary
+                    ) {
+                        print("Action pressed")
+                    }
+                }
+                .padding(.horizontal, 16)
+
+                VGRShape {
+                    VGRCalloutShape(backgroundColor: Color.Primary.blueSurfaceMinimal) {
+                        VGRCalloutText(header: "Hello", description: "World")
+                    }
+                    VGRCalloutShape(backgroundColor: Color.Accent.limeSurface) {
+                        VGRCalloutText(header: "Hello", description: "World")
+                    }
+                }
+            }
         }
-        VGRCalloutShape(backgroundColor: Color.Accent.purpleSurfaceMinimal) {
-            VGRCalloutText(header: "Hello", description: "World")
-        }
+        .navigationTitle("VGRCalloutShape")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
