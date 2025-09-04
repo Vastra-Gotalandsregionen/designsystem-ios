20203ab56aacaac601cd39fff14868c39017821f
Emmanuel Ay <emmanuel.ay@vgregion.se>
2025-09-04 14:09:09 +0200

    Added most common font styles

--- Sources/designsystem/Extensions/Font+Extensions.swift
+++ Sources/designsystem/Extensions/Font+Extensions.swift
@@ -1,8 +1,19 @@
-//
-//  File.swift
-//  designsystem
-//
-//  Created by Emmanuel Ay on 2025-09-04.
-//
+import SwiftUI
 
-import Foundation
+public extension Font {
+
+    // MARK: - Body
+    static var bodyRegular: Font { .body.weight(.regular) }
+    static var bodyMedium: Font { .body.weight(.medium) }
+    static var bodyBold: Font { .body.weight(.bold) }
+    static var bodySemibold: Font { .body.weight(.semibold) }
+
+    // MARK: - Footnote
+    static var footnoteRegular: Font { .footnote.weight(.regular) }
+    static var footnoteSemibold: Font { .footnote.weight(.semibold) }
+    static var footnoteBold: Font { .footnote.weight(.bold) }
+
+    // MARK: - Headline
+    static var headlineSemibold: Font { .headline.weight(.semibold) }
+
+}
