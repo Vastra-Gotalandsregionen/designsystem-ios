f88a314eee10e133265775bd98a7b4e8e3af7b44
Emmanuel Ay <emmanuel@block21.com>
2025-09-18 10:04:23 +0200

    Deprecate old view modifier (#54)
    Co-authored-by: Emmanuel Ay <emmanuel.ay@vgregion.se>

--- Sources/designsystem/Logic/Matomo/TrackerViewModifier.swift
+++ Sources/designsystem/Logic/Matomo/TrackerViewModifier.swift
@@ -1,5 +1,32 @@
 import SwiftUI
 
+public extension View {
+    func track(_ screen: TrackableScreen) -> some View {
+        modifier(TrackableScreenViewModifier(trackableScreen: screen))
+    }
+}
+
+public struct TrackableScreenViewModifier: ViewModifier {
+    var trackableScreen: TrackableScreen
+
+    public func body(content: Content) -> some View {
+        content
+            .onAppear {
+                Tracker.shared.trackScreen(trackableScreen)
+            }
+    }
+}
+
+//MARK: - Deprecated
+
+public extension View {
+    @available(*, deprecated, message: "Use track(_ screen: TrackableScreen) instead.")
+    func track(_ screen: TrackerScreen) -> some View {
+        modifier(TrackerViewModifier(trackerScreen: screen))
+    }
+}
+
+@available(*, deprecated, message: "Do not use this modifier, use the one that accepts a TrackableScreen instead.")
 public struct TrackerViewModifier: ViewModifier {
     var trackerScreen: TrackerScreen
 
@@ -11,8 +38,3 @@ public struct TrackerViewModifier: ViewModifier {
     }
 }
 
-public extension View {
-    func track(_ screen: TrackerScreen) -> some View {
-        modifier(TrackerViewModifier(trackerScreen: screen))
-    }
-}
