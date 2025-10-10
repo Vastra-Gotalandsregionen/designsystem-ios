53d6b27cf13ce7f30313e8bd179cad5c6b4ed6df
Emmanuel Ay <emmanuel.ay@vgregion.se>
2025-10-10 13:30:30 +0200

    Changed visibility of VGRCalendarPeriodModel properties

--- Sources/DesignSystem/Views/Pickers/Calendar/VGRCalendarPeriodModel.swift
+++ Sources/DesignSystem/Views/Pickers/Calendar/VGRCalendarPeriodModel.swift
@@ -1,9 +1,14 @@
 import Foundation
 
 public struct VGRCalendarPeriodModel: Identifiable, Hashable {
+    /// Unique identifier for the period.
     public var id: UUID = UUID()
-    var idx: VGRCalendarIndexKey
-    var days: [VGRCalendarIndexKey]
+
+    /// The index key representing this period in the calendar.
+    public var idx: VGRCalendarIndexKey
+
+    /// The list of days (as index keys) that belong to this period.
+    public var days: [VGRCalendarIndexKey]
 
     /// Used to pad out the monthViews first days, in case the month starts on anything else than a monday
     var leadingPadding: Int
