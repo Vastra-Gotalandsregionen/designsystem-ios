4524c78ebd4ca1b07b9f314f117aaf6e143c1bee
Emmanuel Ay <emmanuel@block21.com>
2026-03-26 11:47:09 +0100

    enum visibility (#128)
    Co-authored-by: Emmanuel Ay <emmanuel.ay@vgregion.se>

--- Sources/DesignSystem/Extensions/CGFloat+ThemeValues.swift
+++ Sources/DesignSystem/Extensions/CGFloat+ThemeValues.swift
@@ -1,28 +1,28 @@
 import Foundation
 
-extension CGFloat {
+public extension CGFloat {
 
     enum Radius {
-        static let mainRadius: CGFloat = 26
-        static let screen: CGFloat = 62
-        static let large38: CGFloat = 38
-        static let vgrCorner: CGFloat = 40
-        static let smallSchema: CGFloat = 8
+        public static let mainRadius: CGFloat = 26
+        public static let screen: CGFloat = 62
+        public static let large38: CGFloat = 38
+        public static let vgrCorner: CGFloat = 40
+        public static let smallSchema: CGFloat = 8
     }
 
     enum Margins {
-        static let xtraLarge: CGFloat = 32
-        static let large: CGFloat = 24
-        static let medium: CGFloat = 16
-        static let small: CGFloat = 12
-        static let xtraSmall: CGFloat = 8
+        public static let xtraLarge: CGFloat = 32
+        public static let large: CGFloat = 24
+        public static let medium: CGFloat = 16
+        public static let small: CGFloat = 12
+        public static let xtraSmall: CGFloat = 8
 
-        static let safeArea: CGFloat = 16
+        public static let safeArea: CGFloat = 16
     }
 
     enum Letterspacing {
-        static let small: CGFloat = 0.2
-        static let medium: CGFloat = 0.0
+        public static let small: CGFloat = 0.2
+        public static let medium: CGFloat = 0.0
     }
 
 }
