6464ae0655bbb0345f32d1c8235f1e8c0ede1afb
Emmanuel Ay <emmanuel.ay@vgregion.se>
2026-03-26 11:24:32 +0100

    Added theme values for radius, margins, and letter spacing

---
+++ Sources/DesignSystem/Extensions/CGFloat+ThemeValues.swift
@@ -0,0 +1,18 @@
+extension CGFloat {
+
+    enum Radius {
+        static let mainRadius: CGFloat = 26
+    }
+
+    enum Margins {
+        static let small: CGFloat = 12
+        static let medium: CGFloat = 16
+        static let xtraSmall: CGFloat = 8
+    }
+
+    enum Letterspacing: CGFloat {
+        static let small: CGFloat = 0.2
+        static let medium: CGFloat = 0.0
+    }
+
+}
