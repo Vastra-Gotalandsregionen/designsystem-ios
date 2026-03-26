ea5b960e010156d94fea0ffb66cdd5565acd2c6f
Emmanuel Ay <emmanuel.ay@vgregion.se>
2026-03-26 11:38:56 +0100

    Add additional radius and margin theme values

--- Sources/DesignSystem/Extensions/CGFloat+ThemeValues.swift
+++ Sources/DesignSystem/Extensions/CGFloat+ThemeValues.swift
@@ -4,12 +4,20 @@ extension CGFloat {
 
     enum Radius {
         static let mainRadius: CGFloat = 26
+        static let screen: CGFloat = 62
+        static let large38: CGFloat = 38
+        static let vgrCorner: CGFloat = 40
+        static let smallSchema: CGFloat = 8
     }
 
     enum Margins {
-        static let small: CGFloat = 12
+        static let xtraLarge: CGFloat = 32
+        static let large: CGFloat = 24
         static let medium: CGFloat = 16
+        static let small: CGFloat = 12
         static let xtraSmall: CGFloat = 8
+
+        static let safeArea: CGFloat = 16
     }
 
     enum Letterspacing {
